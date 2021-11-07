e6cf8a7b4079ed5940714ec307be9ddc987fd222
Andrii <maystrenko22a@gmail.com>
2021-11-07 19:26:44 +0200

    redacted

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './components/Section/App.jsx';
+import App from './components/App/App.jsx';
 
 ReactDOM.render(
   <React.StrictMode>
